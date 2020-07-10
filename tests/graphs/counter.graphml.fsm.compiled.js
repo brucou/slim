fa3d5e71aa30e01e4a51ba0709cdd98d524c7c04
b <bruno.couriol@gmail.com>
2020-07-10 15:02:21 +0200

    doc: README, documenting compiled sizes

--- tests/graphs/counter.graphml.fsm.compiled.js
+++ tests/graphs/counter.graphml.fsm.compiled.js
@@ -1,44 +1,95 @@
+// Generated automatically by Kingly, version 0.7
+// http://github.com/brucou/Kingly
+// Copy-paste help
+// For debugging purposes, guards and actions functions should all have a name
+// Using natural language sentences for labels in the graph is valid
+// guard and action functions name still follow JavaScript rules though
+// -----Guards------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns Boolean
+ */
+// const guards = {
+// };
+// -----Actions------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns {{updates: U[], outputs: O[]}}
+ * (such that updateState:: E -> U[] -> E)
+ */
+// const actions = {
+//   "increment counter": function (extendedState, eventData, settings){},
+//   "render": function (extendedState, eventData, settings){},
+// };
+// -------Control states---------
+/*
+      {"0":"nok","1":"idleღn1"}
+      */
+// ------------------------------
+
+function chain(arrFns, actions) {
+  return function chain_(s, ed, stg) {
+    return arrFns.reduce(
+      function (acc, fn) {
+        var r = actions[fn](s, ed, stg);
+
+        return {
+          updates: acc.updates.concat(r.updates),
+          outputs: acc.outputs.concat(r.outputs),
+        };
+      },
+      { updates: [], outputs: [] }
+    );
+  };
+}
+
 function createStateMachine(fsmDefForCompile, stg) {
   var actions = fsmDefForCompile.actionFactories;
-  var guards = fsmDefForCompile.guards;
   var updateState = fsmDefForCompile.updateState;
   var initialExtendedState = fsmDefForCompile.initialExtendedState;
 
   // Initialize machine state,
-  var cs = "nok";
+  // Start with pre-initial state "nok"
+  var cs = 0;
   var es = initialExtendedState;
 
-  var eventHandlers = {
-    nok: {
-      init: function (es, ed, stg) {
-        cs = "n1ღidle"; // No action, only cs changes!
+  var eventHandlers = [
+    {
+      init: function (s, ed, stg) {
+        // Transition to idleღn1;
+        cs = 1; // No action, only cs changes!
 
         return { outputs: [], updates: [] };
       },
     },
-    n1ღidle: {
-      click: function (es, ed, stg) {
-        let computed = actions["increment counter, render"](es, ed, stg);
-        cs = "n1ღidle";
-        es = updateState(es, computed.updates);
+    {
+      click: function (s, ed, stg) {
+        let computed = chain(["increment counter", "render"], actions)(s, ed, stg);
+        // Transition to idleღn1;
+        cs = 1;
+        es = updateState(s, computed.updates);
 
         return computed;
       },
     },
-  };
+  ];
 
   function process(event) {
     var eventLabel = Object.keys(event)[0];
     var eventData = event[eventLabel];
     var controlStateHandlingEvent = (eventHandlers[cs] || {})[eventLabel] && cs;
 
-    if (controlStateHandlingEvent) {
+    if (controlStateHandlingEvent != null) {
       // Run the handler
       var computed = eventHandlers[controlStateHandlingEvent][eventLabel](es, eventData, stg);
 
       // cs, es, hs have been updated in place by the handler
       // If transition, but no guards fulfilled => null, else => computed outputs
-      return computed === null ? null : computed.outputs;
+      return computed === null ? [null] : computed.outputs;
     }
     // Event is not accepted by the machine
     else return null;
--- tests/graphs/password-meter.graphml.fsm.compiled.cjs
+++ tests/graphs/password-meter.graphml.fsm.compiled.cjs
@@ -1,3 +1,40 @@
+// Generated automatically by Kingly, version 0.7
+// http://github.com/brucou/Kingly
+// Copy-paste help
+// For debugging purposes, guards and actions functions should all have a name
+// Using natural language sentences for labels in the graph is valid
+// guard and action functions name still follow JavaScript rules though
+// -----Guards------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns Boolean
+ */
+// const guards = {
+//   "!letter and numbers?": function (extendedState, eventData, settings){},
+//   "letter and numbers?": function (extendedState, eventData, settings){},
+// };
+// -----Actions------
+/**
+ * @param {E} extendedState
+ * @param {D} eventData
+ * @param {X} settings
+ * @returns {{updates: U[], outputs: O[]}}
+ * (such that updateState:: E -> U[] -> E)
+ */
+// const actions = {
+//   "display initial screen": function (extendedState, eventData, settings){},
+//   "display weak password screen": function (extendedState, eventData, settings){},
+//   "display strong password screen": function (extendedState, eventData, settings){},
+//   "display password submitted screen": function (extendedState, eventData, settings){},
+// };
+// -------Control states---------
+/*
+      {"0":"nok","1":"doneღn1","2":"strongღn2","3":"weakღn3","4":"idleღn4"}
+      */
+// ------------------------------
+
 function createStateMachine(fsmDefForCompile, stg) {
   var actions = fsmDefForCompile.actionFactories;
   var guards = fsmDefForCompile.guards;
@@ -5,81 +42,91 @@ function createStateMachine(fsmDefForCompile, stg) {
   var initialExtendedState = fsmDefForCompile.initialExtendedState;
 
   // Initialize machine state,
-  var cs = "nok";
+  // Start with pre-initial state "nok"
+  var cs = 0;
   var es = initialExtendedState;
 
-  var eventHandlers = {
-    n4ღidle: {
-      start: function (es, ed, stg) {
-        let computed = actions["display initial screen"](es, ed, stg);
-        cs = "n3ღweak";
-        es = updateState(es, computed.updates);
-
-        return computed;
-      },
-    },
-    n3ღweak: {
-      typed: function (es, ed, stg) {
-        let computed = null;
-        if (guards["!letter and numbers?"](es, ed, stg)) {
-          computed = actions["display weak password screen"](es, ed, stg);
-          cs = "n3ღweak";
-        } else if (guards["letter and numbers?"](es, ed, stg)) {
-          computed = actions["display strong password screen"](es, ed, stg);
-          cs = "n2ღstrong";
-        }
-        if (computed !== null) {
-          es = updateState(es, computed.updates);
-        }
-
-        return computed;
-      },
-    },
-    n2ღstrong: {
-      typed: function (es, ed, stg) {
-        let computed = null;
-        if (guards["letter and numbers?"](es, ed, stg)) {
-          computed = actions["display strong password screen"](es, ed, stg);
-          cs = "n2ღstrong";
-        } else if (guards["!letter and numbers?"](es, ed, stg)) {
-          computed = actions["display weak password screen"](es, ed, stg);
-          cs = "n3ღweak";
-        }
-        if (computed !== null) {
-          es = updateState(es, computed.updates);
-        }
-
-        return computed;
-      },
-      "clicked submit": function (es, ed, stg) {
-        let computed = actions["display password submitted screen"](es, ed, stg);
-        cs = "n1ღdone";
-        es = updateState(es, computed.updates);
-
-        return computed;
-      },
-    },
-    nok: {
-      init: function (es, ed, stg) {
-        cs = "n4ღidle"; // No action, only cs changes!
+  var eventHandlers = [
+    {
+      init: function (s, ed, stg) {
+        // Transition to idleღn4;
+        cs = 4; // No action, only cs changes!
 
         return { outputs: [], updates: [] };
       },
     },
-  };
+    ,
+    {
+      typed: function (s, ed, stg) {
+        let computed = null;
+        if (guards["letter and numbers?"](s, ed, stg)) {
+          computed = actions["display strong password screen"](s, ed, stg);
+          // Transition to "strongღn2";
+          cs = 2;
+        } else if (guards["!letter and numbers?"](s, ed, stg)) {
+          computed = actions["display weak password screen"](s, ed, stg);
+          // Transition to "weakღn3";
+          cs = 3;
+        }
+        if (computed !== null) {
+          es = updateState(s, computed.updates);
+        }
+
+        return computed;
+      },
+
+      "clicked submit": function (s, ed, stg) {
+        let computed = actions["display password submitted screen"](s, ed, stg);
+        // Transition to doneღn1;
+        cs = 1;
+        es = updateState(s, computed.updates);
+
+        return computed;
+      },
+    },
+    {
+      typed: function (s, ed, stg) {
+        let computed = null;
+        if (guards["!letter and numbers?"](s, ed, stg)) {
+          computed = actions["display weak password screen"](s, ed, stg);
+          // Transition to "weakღn3";
+          cs = 3;
+        } else if (guards["letter and numbers?"](s, ed, stg)) {
+          computed = actions["display strong password screen"](s, ed, stg);
+          // Transition to "strongღn2";
+          cs = 2;
+        }
+        if (computed !== null) {
+          es = updateState(s, computed.updates);
+        }
+
+        return computed;
+      },
+    },
+    {
+      start: function (s, ed, stg) {
+        let computed = actions["display initial screen"](s, ed, stg);
+        // Transition to weakღn3;
+        cs = 3;
+        es = updateState(s, computed.updates);
+
+        return computed;
+      },
+    },
+  ];
 
   function process(event) {
     var eventLabel = Object.keys(event)[0];
     var eventData = event[eventLabel];
     var controlStateHandlingEvent = (eventHandlers[cs] || {})[eventLabel] && cs;
 
-    if (controlStateHandlingEvent) {
+    if (controlStateHandlingEvent != null) {
       // Run the handler
       var computed = eventHandlers[controlStateHandlingEvent][eventLabel](es, eventData, stg);
 
       // cs, es, hs have been updated in place by the handler
       // If transition, but no guards fulfilled => null, else => computed outputs
-      return computed === null ? null : computed.outputs;
+      return computed === null ? [null] : computed.outputs;
     }
     // Event is not accepted by the machine
     else return null;
